fabe6dcc06c84b9f37505f12e8d0875d490d1d13
enzogzz <enzo@egonzalez.fr>
2023-02-09 15:31:49 +0100

    feat(lab1): updated check values for compatibility

--- lab1/src/spaceship/reactors/check-script.sh
+++ lab1/src/spaceship/reactors/check-script.sh
@@ -5,7 +5,7 @@ GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
 
-if [ "$(cat right-reactor/engine.conf 2>/dev/null  | tr -d '\n')" == "fuel-type=tritiumquantity=39KBengine-with-starter=1" ]; then
+if [[ "$(cat right-reactor/engine.conf 2>/dev/null  | tr -d '\n')" == "fuel-type=tritiumquantity=39KBengine-with-starter=1" || "$(cat right-reactor/engine.conf 2>/dev/null  | tr -d '\n')" == "fuel-type=tritiumquantity=40KBengine-with-starter=1" ]]; then
   echo -e "Br br BRbrb rbrbr ...br brbR.B.br .b brRRRRRRTRTRTRTR...\n${GREEN}Reactors works! Well done soldier.${NC}"
 else
   echo -e "Br br BRbrb rbrbr ...br brbR.B.br..Glouu glouuuu...\n${YELLOW}Well, that's a fail..${NC}"
