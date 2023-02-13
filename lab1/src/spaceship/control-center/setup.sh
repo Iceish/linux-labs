7e86150bc0b7abd107be378a5edd077586d44a16
enzogzz <enzo@egonzalez.fr>
2023-02-13 16:34:10 +0100

    feat(lab1): control-center upgrade ergonomic

--- lab1/src/spaceship/control-center/setup.sh
+++ lab1/src/spaceship/control-center/setup.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
 if [ "$EUID" -ne 0 ]
-  then echo "Please run as root / sudo"
-  exit
+  then echo "Please run this script as root or with sudo command."
+  exit 1
 fi
 
-chmod 666 unlock.sh
+cat ./.launchSetupToStart/unlock.sh.enc | base64 -d > unlock.sh
+cp ./.launchSetupToStart/control.log.enc ./
+mv ./setup.sh ./.launchSetupToStart/
 
 useradd h4cker-lab
-chmod 000 control.log
 chown h4cker-lab:h4cker-lab control.log
+
+chmod 000 unlock.sh
+chmod 000 control.log
+
+echo "================================"
+echo "|         Setup Script         |"
+echo "================================"
+echo ""
+
+echo "This room is ready."
+echo "(In case of malfunction, setup.sh is in .launchSetupToStart folder. Copy this file back and launch it.)"
