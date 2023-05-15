4eac52586977c3f7ccd72fdb6122c2a919a9680b
enzogzz <enzo@egonzalez.fr>
2023-05-15 12:10:50 +0200

    feat: add help to lab cmd

--- src/spaceship/.backend/index.sh
+++ src/spaceship/.backend/index.sh
@@ -1,21 +1,46 @@
 #/bin/bash
 
-if [ "$1" == "check_event" ]; then
-	diff "event_landing_report.txt" "./.backend/event_landing_report.anwser" &> /dev/null
-	if [ "$?" -eq "0" ]; then
+cmd="$1"
+case "$cmd" in
+
+
+	"check_event" )
+		diff "event_landing_report.txt" "./.backend/event_landing_report.anwser" &> /dev/null
+		if [ "$?" -eq "0" ]; then
+			echo "succed";
+		else
+			echo "failed";
+		fi
+	;;
+
+
+	"check_count")
+		diff "count_landing_report.txt" "./.backend/count_landing_report.anwser" &> /dev/null
+		if [ "$?" -eq "0" ]; then
 		echo "succed";
-	else
+		else
 		echo "failed";
-	fi
+		fi
+	;;
 
-fi
+	"help")
+		echo "Lab command"
+		echo ""
+		echo "DESCRIPTION"
+		echo "This is an alias that launch '.backend\\index.sh file with parameters.'"
+		echo ""
+		echo "SYNTAX"
+		echo "lab <cmd>"
+		echo ""
+		echo "OPTIONS"
+		echo "- help : Display this help page."
+		echo "- check_event : Check if landing event report is correct."
+		echo "- check_count : Check if landing count report is correct."
+	;;
 
-if [ "$1" == "check_count" ]; then
-	diff "count_landing_report.txt" "./.backend/count_landing_report.anwser" &> /dev/null
-	if [ "$?" -eq "0" ]; then
-		echo "succed";
-	else
-		echo "failed";
-	fi
+	*)
+		echo "'$cmd' option does not exists."
+		echo "Type 'lab help' to get further informations."
+		;;
+esac
 
-fi
