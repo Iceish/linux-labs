8d245e68997a3deeda2f00bf3c63974510842acc
enzogzz <enzo@egonzalez.fr>
2023-05-15 11:52:21 +0200

    feat: update check script

--- src/spaceship/.backend/index.sh
+++ src/spaceship/.backend/index.sh
@@ -1,7 +1,17 @@
 #/bin/bash
 
-if [ "$1" == "check" ]; then
-	diff "logs.txt" "./.backend/logs_res.txt" &> /dev/null
+if [ "$1" == "check_event" ]; then
+	diff "event_landing_report.txt" "./.backend/event_landing_report.anwser" &> /dev/null
+	if [ "$?" -eq "0" ]; then
+		echo "succed";
+	else
+		echo "failed";
+	fi
+
+fi
+
+if [ "$1" == "check_count" ]; then
+	diff "count_landing_report.txt" "./.backend/count_landing_report.anwser" &> /dev/null
 	if [ "$?" -eq "0" ]; then
 		echo "succed";
 	else
