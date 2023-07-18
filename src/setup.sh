d7ca16c4555a9ca88efac0d92720b128708a7b31
enzogzz <enzo@egonzalez.fr>
2023-07-18 14:16:22 +0200

    feat: update setup style

--- src/setup.sh
+++ src/setup.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
+BOLD="\e[1m"
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+CYAN="\e[36m"
+MAGENTA="\e[95m"
+DIM="\e[2m"
+RESET="\e[0m"
+
+
+echo -en "$BOLD"
+echo "===================="
+echo "=   LNX-LAB TOOL   ="
+echo "===================="
+echo -e "$RESET"
+
 [[ "${BASH_SOURCE[0]}" != "${0}" ]] || {
-	echo "/!\ You have to source this script."
+	echo -e "${RED}⚠️  You have to source this script.${RESET}"
 	echo "> source setup.sh"
 	exit 1
 }
@@ -9,6 +25,6 @@
 export LAB_BACKEND="$(pwd)/.backend"
 alias lab=$LAB_BACKEND/index.sh
 
-echo "You successfully initialized this linux lab."
-echo "Enjoy !"
-echo "- EnzoGz"
+echo -e "${GREEN}✅ You successfully initialized this linux lab."
+echo -e "Enjoy !${RESET}"
+echo "${DIM}- EnzoGz${RESET}"
